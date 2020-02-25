ccefc7027e1395fd0141088999bac5d4ba42c0c5
Josef Malý <josefmalyy@gmail.com>
2020-02-25 22:18:54 +0100

    :wrench:

--- .theos/obj/debug/arm64/Tweak.xm.mm
+++ .theos/obj/debug/arm64/Tweak.xm.mm
@@ -1,7 +1,16 @@
 #line 1 "Tweak.xm"
+int reachabilityHeight = 200;
+
 @interface SBReachabilityWindow : UIView
     -(id)view;
 @end
+
+@interface SBReachabilitySettings {
+double _yOffsetFactor;
+}
++(id)settingsControllerModule;
+@end
+
 
 
 #include <substrate.h>
@@ -24,31 +33,37 @@
 #define _LOGOS_RETURN_RETAINED
 #endif
 
-@class SBReachabilityWindow; 
+@class UITableView; @class SBReachabilityWindow; 
 static id (*_logos_orig$_ungrouped$SBReachabilityWindow$view)(_LOGOS_SELF_TYPE_NORMAL SBReachabilityWindow* _LOGOS_SELF_CONST, SEL); static id _logos_method$_ungrouped$SBReachabilityWindow$view(_LOGOS_SELF_TYPE_NORMAL SBReachabilityWindow* _LOGOS_SELF_CONST, SEL); 
 
-#line 5 "Tweak.xm"
+#line 14 "Tweak.xm"
 
     static id _logos_method$_ungrouped$SBReachabilityWindow$view(_LOGOS_SELF_TYPE_NORMAL SBReachabilityWindow* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd) {
+        
         id Rview = _logos_orig$_ungrouped$SBReachabilityWindow$view(self, _cmd);
 
-        CGRect rect = CGRectMake(0, -360, 240, 375);
+        CGRect rect = CGRectMake(0, -375/1.5+11, 667, 375/1.5-11);
+
 
         UIView *myBox = [[UIView alloc] initWithFrame:rect];
-		myBox.backgroundColor = [UIColor lightGrayColor];
+		myBox.backgroundColor = [UIColor whiteColor];
     	[Rview addSubview:myBox];
 
-
-
+        UITableView *tblView = [[UITableView alloc]initWithFrame:rect];
+        [Rview addSubview:tblView];
 
         
         
         
         
 
-        return Rview;
+        return Rview;   
     }
+
+
+ 
+
 
 static __attribute__((constructor)) void _logosLocalInit() {
 {Class _logos_class$_ungrouped$SBReachabilityWindow = objc_getClass("SBReachabilityWindow"); MSHookMessageEx(_logos_class$_ungrouped$SBReachabilityWindow, @selector(view), (IMP)&_logos_method$_ungrouped$SBReachabilityWindow$view, (IMP*)&_logos_orig$_ungrouped$SBReachabilityWindow$view);} }
-#line 26 "Tweak.xm"
+#line 41 "Tweak.xm"
--- Tweak.xm
+++ Tweak.xm
@@ -1,25 +1,42 @@
+#import <tableViewTest.m>
+
+int reachabilityHeight = 200;
+
 @interface SBReachabilityWindow : UIView
     -(id)view;
 @end
 
+@interface SBReachabilitySettings {
+double _yOffsetFactor;
+}
++(id)settingsControllerModule;
+@end
+
+
 %hook SBReachabilityWindow
     -(id)view {
+        
         id Rview = %orig;
 
-        CGRect rect = CGRectMake(0, -360, 240, 375);
+        CGRect rect = CGRectMake(0, -375/1.5+11, 667, 375/1.5-11);
+
 
         UIView *myBox = [[UIView alloc] initWithFrame:rect];
-		myBox.backgroundColor = [UIColor lightGrayColor];
+		myBox.backgroundColor = [UIColor whiteColor];
     	[Rview addSubview:myBox];
 
-
-
+        UITableView *tblView = [[UITableView alloc]initWithFrame:rect];
+        [Rview addSubview:tblView];
 
         // UILabel *myLabel = [[UILabel alloc]initWithFrame: rect];
         // [myLabel setBackgroundColor:[UIColor clearColor]];
         // [myLabel setText:@"Smrdíš"];
         // [Rview addSubview:myLabel];
 
-        return Rview;
+        return Rview;   
     }
+%end
+
+%hook UITableView 
+
 %end
